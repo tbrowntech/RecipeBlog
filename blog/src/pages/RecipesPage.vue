b3b83309626951d2128d27978664478131561a8b
Taylor Brown <ataylorbrown91@gmail.com>
2023-08-29 16:24:36 -0600

    adjust formatting of cards

--- blog/src/pages/RecipesPage.vue
+++ blog/src/pages/RecipesPage.vue
@@ -2,22 +2,24 @@
   <div class="container mt-4">
     <h1 class="text-center mb-4 text-aqua">Welcome to My Recipe Blog</h1>
     <div class="row">
-      <div class="col-md-4 mb-4" v-for="post in blogPosts" :key="post.id">
-        <div class="card text-aqua">
-          <img
-            :src="require(`@/assets/${post.imageFileName}`)"
-            class="card-img-top blog-thumbnail mx-auto"
-            alt="recipe image"
-          />
-          <div class="card-body text-center">
-            <h2 class="card-title text-30bfbf">{{ post.title }}</h2>
-            <p class="card-text">{{ post.excerpt }}</p>
-            <router-link
-              :to="{ name: 'PostDetails', params: { id: +post.id } }"
-              class="btn btn-primary"
-            >
-              Read More
-            </router-link>
+      <div class="card-grid">
+        <div class="col-md-4 mb-4" v-for="post in blogPosts" :key="post.id">
+          <div class="card text-aqua">
+            <img
+              :src="require(`@/assets/${post.imageFileName}`)"
+              class="card-img-top blog-thumbnail mx-auto"
+              alt="recipe image"
+            />
+            <div class="card-body text-center">
+              <h2 class="card-title text-30bfbf">{{ post.title }}</h2>
+              <p class="card-text">{{ post.excerpt }}</p>
+              <router-link
+                :to="{ name: 'PostDetails', params: { id: +post.id } }"
+                class="btn btn-primary"
+              >
+                Read More
+              </router-link>
+            </div>
           </div>
         </div>
       </div>
@@ -33,7 +35,7 @@ export default {
       blogPosts: [
         {
           id: 1,
-          title: "Swedish Meatball Sauce",
+          title: "Swedish Meatballs",
           excerpt: "Delicious sauce to put over meatballs, pasta, etc.",
           imageFileName: "SwedishMeatballs.jpg",
           // content: "Sauce: ...",
